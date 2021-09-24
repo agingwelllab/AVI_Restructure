8371ee26d49a88c75a118bf4d5fdad9c50cee6b0
Seaman <kls190004@cvltechs-iMac-3.local>
2021-09-24 13:26:45 -0500

    Change absolute path for functions

--- clean_AVI_data.R
+++ clean_AVI_data.R
@@ -3,12 +3,16 @@
 
 # load required packages
 library(tidyverse)
+library(here)
 
 # load source functions
-source('/Users/ssp160230/github_directory/AVI_Restructure/src/AVI_functions.R')
+#source('/Users/ssp160230/github_directory/AVI_Restructure/src/AVI_functions.R') # this is an absolute path - it only works on YOUR computer
+source(here::here('src', 'AVI_functions.R')) # this makes it a relative path to R Project so anyone with this repo can use it.
 
 # set hard-coded variables 
 avi_raw <- read_csv('/Users/ssp160230/github_directory/data/AVI_data.csv')
+
+#
 
 # clean and reformat raw data frame
 avi_clean <- aclean(avi_raw)
