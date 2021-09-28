23247cfeec98a356e4690492efddc2fdc8aa1797
sspill <ssp160230@utdallas.edu>
2021-09-28 14:14:08 -0500

    looked at pull request notes, made minor edits

--- clean_AVI_data.R
+++ clean_AVI_data.R
@@ -1,19 +1,16 @@
 # Clean AVI data
-# 9.17.21 SP
+# 9.28.21 SP
 
 # load required packages
 library(tidyverse)
 library(here)
 
 # load source functions
-#source('/Users/ssp160230/github_directory/AVI_Restructure/src/AVI_functions.R') # this is an absolute path - it only works on YOUR computer
-source(here::here('src', 'AVI_functions.R')) # this makes it a relative path to R Project so anyone with this repo can use it.
+source(here::here('src', 'AVI_functions.R'))
 
-# set hard-coded variables - should load data in a different section
-#avi_raw <- read_csv('/Users/ssp160230/github_directory/data/AVI_data.csv') 
-#another absolute path - if you use any absolute paths, they should definitely be saved here as variables; data should be loaded separately
+# set hard-coded variables
 
-#load data
+# load data
 avi_raw <- read.csv(here::here('data', 'AVI_data.csv'))
 
 # clean and reformat raw data frame
@@ -21,6 +18,3 @@ avi_clean <- aclean(avi_raw)
 
 # calculate mean octant scores for each participant
 avi_octant <- aoctant(avi_clean)
-
-
-
